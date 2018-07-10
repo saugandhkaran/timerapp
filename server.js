d1e6537c92bcf744d06c6b5688bb49a753da3192
saugandhkaran <saugandhkaran@gmail.com>
2018-07-10 20:37:53 +0200

    Add files via upload
    Changed file path in server.js

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+//app.use(express.static(__dirname + '/dist'));
+app.use(express.static(path.join(__dirname, 'dist/timerapp')));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/timerapp/index.html'));
+res.sendFile(path.join(__dirname+'/index.html'));
 });
-console.log(__dirname+'/dist/timerapp/index.html');
+console.log(__dirname+'/index.html');
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
